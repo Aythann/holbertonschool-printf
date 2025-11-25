892de084ba501a0e2564d9b2432d88ae9d1d74b1
Vincent-12156 <12156@holbertonstudents.com>
2025-11-25 09:33:20 +0100

    Fixe the code of variadic funct

--- 3-print_all.c
+++ 3-print_all.c
@@ -45,9 +45,7 @@ void print_s(va_list ap)
 
 /**
  * print_all - prints anything
- * @format: list of types of arguments
- *
- * Return: void
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
@@ -67,94 +65,13 @@ void print_all(const char * const format, ...)
 		while (types[j] && types[j] != format[i])
 			j++;
 
-		if (types[j])
-		{
-			printf("%s", sep);
-			funcs[j](ap);
-			sep = ", ";
-		}#include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
-
-/**
- * print_c - print char
- * @ap: argument list
- */
-void print_c(va_list ap)
-{
-	printf("%c", (char)va_arg(ap, int));
-}
-
-/**
- * print_i - print int
- * @ap: argument list
- */
-void print_i(va_list ap)
-{
-	printf("%d", va_arg(ap, int));
-}
-
-/**
- * print_f - print float
- * @ap: argument list
- */
-void print_f(va_list ap)
-{
-	printf("%f", va_arg(ap, double));
-}
-
-/**
- * print_s - print string
- * @ap: argument list
- */
-void print_s(va_list ap)
-{
-	char *s = va_arg(ap, char *);
-
-	if (!s)
-		s = "(nil)";
-
-	printf("%s", s);
-}
-
-/**
- * print_all - prints anything
- * @format: list of types of arguments
- *
- * Return: void
- */
-void print_all(const char * const format, ...)
-{
-	va_list ap;
-	int i = 0, j;
-	char *types = "cifs";
-	char *sep = "";
-
-	void (*funcs[])(va_list) = {print_c, print_i, print_f, print_s};
-
-	va_start(ap, format);
-
-	while (format && format[i])
-	{
-		j = 0;
-
-		while (types[j] && types[j] != format[i])
-			j++;
-
+        /* If format[i] matches a valid type */
 		if (types[j])
 		{
 			printf("%s", sep);
 			funcs[j](ap);
 			sep = ", ";
 		}
-
-		i++;
-	}
-
-	va_end(ap);
-	printf("\n");
-}
-
 
 		i++;
 	}

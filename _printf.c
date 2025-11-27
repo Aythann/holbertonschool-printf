d31816f7964c2817560c925c9b11e88a61fc6152
romain344 <romainleoulzec@gmail.com>
2025-11-27 12:45:21 +0100

    feat: add core fifes for custom _printf implementation

--- _printf.c
+++ _printf.c
@@ -14,40 +14,39 @@ int _printf(const char *format, ...)
 	int result = 0;
 	int type;
 
-		if (format == NULL)
+	if (format == NULL)
 		return (-1);
 
-		va_start(ap, format);
-		while (format[i] != '\0')
+	va_start(ap, format);
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
-	{
-		i++;
-		if (format[i] == '\0')
 		{
-		va_end(ap);
-		return (-1);
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(ap);
+				return (-1);
+			}
+			type = parse_format(format[i]);
+			if (type == TYPE_CHAR)
+				result += print_char(va_arg(ap, int));
+			else if (type == TYPE_STRING)
+				result += print_string(va_arg(ap, char *));
+			else if (type == TYPE_PERCENT)
+				result += print_percent();
+			else if (type == TYPE_INT)
+				result += print_int(va_arg(ap, int));
+			else
+			{
+				result += _putchar('%');
+				result += _putchar(format[i]);
+			}
 		}
-		type = parse_format(format[i]);
-		if (type == TYPE_CHAR)
-		result += print_char(va_arg(ap, int));
-		else if (type == TYPE_STRING)
-		result += print_string(va_arg(ap, char *));
-		else if (type == TYPE_PERCENT)
-		result += print_percent();
-		else if (type == TYPE_INT)
-		result += print_int(va_arg(ap, int));
 		else
-		{
-		result += _putchar('%');
-		result += _putchar(format[i]);
-		}
-	}
-		else
-		result += _putchar(format[i]);
+			result += _putchar(format[i]);
 		i++;
 	}
 	va_end(ap);
 	return (result);
 }
-

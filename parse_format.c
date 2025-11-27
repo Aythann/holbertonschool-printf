f08fc847721eb847843c5162b8f615b0c385a691
romain344 <romainleoulzec@gmail.com>
2025-11-27 10:08:08 +0100

    feat: add core fifes for custom _printf implementation

--- parse_format.c
+++ parse_format.c
@@ -1,87 +1,21 @@
 #include "main.h"
 
 /**
- * _print_number - prints a number recursively
- * @n: number to print
+ * parse_format - checks which format specifier is used
+ * @c: the character after '%'
  *
- * Return: number of chars printed
+ * Return: a code corresponding to the format type
  */
-int _print_number(long n)
+int parse_format(char c)
 {
-	int count = 0;
-	char digit;
+	if (c == 'c')
+		return (TYPE_CHAR);
+	else if (c == 's')
+		return (TYPE_STRING);
+	else if (c == '%')
+		return (TYPE_PERCENT);
+	else if (c == 'd' || c == 'i')
+		return (TYPE_INT);
 
-	if (n < 0)
-	{
-		count += write(1, "-", 1);
-		n = -n;
-	}
-
-	if (n / 10)
-		count += _print_number(n / 10);
-
-	digit = (n % 10) + '0';
-	count += write(1, &digit, 1);
-
-	return (count);
-}
-
-/**
- * _print_int - prints an integer (%d, %i)
- * @ap: argument list
- *
- * Return: number of chars printed
- */
-int _print_int(va_list ap)
-{
-	long n = va_arg(ap, int);
-
-	return (_print_number(n));
-}
-
-/**
- * _print_char - Prints a single character (c)
- * @ap: List of arguments containing the character
- *
- * Return: Number of characters printed (1)
- */
-
-int _print_char(va_list ap)
-{
-	char c = (char)va_arg(ap, int);
-
-	return (write(1, &c, 1));
-}
-
-/**
- * _print_string - Prints a string (s)
- * @ap: List of arguments containing the string
- *
- * Return: Number of characters printed
- */
-
-int _print_string(va_list ap)
-{
-	char *str = va_arg(ap, char *);
-	int count = 0;
-
-	if (str == NULL)
-	str = "(null)";
-
-	while (*str)
-	count += write(1, str++, 1);
-
-	return (count);
-}
-
-/**
- * _print_percent - Prints a percent sign (%)
- * @ap: Unused parameter
- *
- * Return: Always 1
- */
-
-int _print_percent(__attribute__((unused)) va_list ap)
-{
-	return (write(1, "%", 1));
+	return (-1);
 }

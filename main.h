2acfaf65fa89691794edb3b5374ed118ceba23f8
Vincent-12156 <12156@holbertonstudents.com>
2025-11-28 10:45:39 +0100

    Fixed library in main.c ang long format in arg_funct.c and _printf.c

--- main.h
+++ main.h
@@ -3,15 +3,10 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-
-#define TYPE_CHAR 1
-#define TYPE_STRING 2
-#define TYPE_PERCENT 3
-#define TYPE_INT 4
+#include <stdlib.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int parse_format(char c);
 int print_char(char c);
 int print_string(char *s);
 int print_percent(void);
